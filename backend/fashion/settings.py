cc81304e0e673bbfb19d5e672badaf44f513e33e
HyunAm <tlfgjawlq@naver.com>
2021-05-15 20:36:54 +0900

    Feat : Docker Setting

--- backend/fashion/settings.py
+++ backend/fashion/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'movie_project', 'static'),
+    os.path.join(BASE_DIR, 'fashion', 'static'),
 ]
 
 MEDIA_URL = '/media/'
