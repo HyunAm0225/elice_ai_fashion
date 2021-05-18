a56d3b94dbd8aed94cfb11d2b195e8d7f9ada6d6
HyunAm <tlfgjawlq@naver.com>
2021-05-18 15:32:00 +0900

    Feat : user model 구현

--- backend/fashion/settings.py
+++ backend/fashion/settings.py
@@ -139,3 +139,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'user.User'
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    nickname = models.CharField('nickname', max_length=40, default='')
+    # 성별 선언해주기
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    CHOICES_GENDER = (
+        (GENDER_MALE, '남성'),
+        (GENDER_FEMALE, '여성'),
+    )
+    gender = models.CharField(max_length=10, choices=CHOICES_GENDER, null=True)
+
+    @property
+    def name(self):
+        return f"{self.last_name}{self.first_name}"
+
+    REQUIRED_FIELDS = ['nickname']
+
+    class meta:
+        db_table = 'users'
