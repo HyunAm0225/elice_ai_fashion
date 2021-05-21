4eb7a00827e48add13be342f68a2d83284580fad
HyunAm <tlfgjawlq@naver.com>
2021-05-21 20:54:28 +0900

    Fix : swagger url 수정

--- backend/fashion/urls.py
+++ backend/fashion/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import routers
+from django.conf.urls import url
 from rest_framework.permissions import AllowAny
 # 스웨거 설정
 from drf_yasg.views import get_schema_view
@@ -39,9 +40,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     # 스웨거 설정
-    path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
     # 장고 앱
     path('admin/', admin.site.urls),
     path('api/', include('user.urls', namespace='user')),
