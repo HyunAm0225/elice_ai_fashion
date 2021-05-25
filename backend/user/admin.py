76f047ba9b3bc8d1620745419c0e78805feb44e9
HyunAm <tlfgjawlq@naver.com>
2021-05-25 16:45:44 +0900

    Feat : closet url CRUD기능 추가

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -3,12 +3,18 @@ from .models import User, Closet
 # Register your models here.
 
 
+class ClosetInLine(admin.StackedInline):
+    model = Closet
+    extra = 2
+
+
 class UserAdmin(admin.ModelAdmin):
+    inlines = (ClosetInLine,)
     list_display = ('username', 'full_name', 'name', 'gender',)
 
 
 class ClosetAdmin(admin.ModelAdmin):
-    list_display = ('dress_img',)
+    list_display = ('id', 'user_id', 'dress_img',)
 
 
 admin.site.register(User, UserAdmin)
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -8,13 +8,15 @@ from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
 
-router = routers.DefaultRouter()
 app_name = 'user'
+router = routers.DefaultRouter()
+router.register(r'closet', views.ClosetViewSet)
 # router.register('user', views.UserCreateSerializer)
 
 urlpatterns = [
     # 장고 앱
-    path('signup', views.SignupView.as_view(), name='signup'),
-    path('login', obtain_jwt_token),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('login/', obtain_jwt_token, name='login'),
+    path('', include(router.urls)),
     # path('logout/', refresh_jwt_token),
 ]
