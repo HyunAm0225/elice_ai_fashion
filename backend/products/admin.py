6f47301a0347cacebaec2821f23d8b8fb70edac8
HyunAm <tlfgjawlq@naver.com>
2021-05-28 16:40:37 +0900

    Feat : 상품안에서 좋아요 관리(Admin)

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -2,11 +2,23 @@ from django.contrib import admin
 from .models import Product, LikeProduct
 
 
+class LikeInline(admin.TabularInline):
+    model = Product.likeproduct.through
+    verbose_name = "like"
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'brand', 'category', 'discount_rate')
+    list_display = ('id', 'name', 'brand', 'category', 'discount_rate')
+    search_fields = ['name']
+    inlines = (LikeInline,)
+
+    def __str__(self):
+        return self.name
+
 
 class LikeProductAdmin(admin.ModelAdmin):
-    list_display = ('user_id','product_id','is_like')
-    
+    list_display = ('user_id', 'product_id', 'is_like')
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(LikeProduct, LikeProductAdmin)
