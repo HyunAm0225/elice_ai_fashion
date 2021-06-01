2273cf9649c1107b17d06fc98802b61861a18ca0
hyunguclear <jhg3522@kookmin.ac.kr>
2021-06-02 01:42:15 +0900

    Feat shopbob crawler

--- backend/crawling.py
+++ backend/crawling.py
@@ -1,16 +1,80 @@
 from django.shortcuts import render
-import requests
-import os
-import urllib.request
+import requests, os, urllib.request, time, random, ssl
 from urllib.parse import urlunparse
 from bs4 import BeautifulSoup
-import time
-import random
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fashion.settings")
 import django
 django.setup()
 from products.models import Product
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# short sleeve top
+# long sleeve top
+# short sleeve outwear
+# long sleeve outwear
+# vest
+# sling
+# shorts
+# trousers
+# skirt
+# short sleeve dress
+# long sleeve dress
+# vest dress
+# sling dress
+
+kategorie_data = {
+"coats": ["64892","long sleeve outwear"],
+"dresses": ["15412","long sleeve dress"],
+"jackets": ["64849","long sleeve outwear"],
+"jeans": ["15382","trousers"],
+"pants": ["15474","trousers"],
+"shorts": ["15486","shorts"],
+"skirts": ["15491","skirt"],
+'sweaters-knits': ["15505","long sleeve top"],
+"tops": ["15522","long sleeve top"]
+}
+
+color_data= ['white', 'grey', 'black', 'beige', 'brwon',' blue', 'navy', 'purple', 'green', 'red', 'orange', 'yellow', 'pink']
+
+def get_cloth_data():
+    number = [get_cloth_count(kategorie) for kategorie in kategorie_data]
+    print(number)
+    n = 1
+    # test = 0
+    for idx,kategorie in enumerate(kategorie_data):
+        for i in range(number[idx]):
+            result = requests.get(f"https://www.shopbop.com/sale-clothing-{kategorie}/br/v=1/{kategorie_data[kategorie][0]}.htm?baseIndex={i*100}")
+            # Check for 200 OK response
+            src = result.content
+            soup = BeautifulSoup(src, 'lxml')
+            ul = soup.find("ul", {'class': 'products'})
+            for li in ul.find_all("li"):
+                # print(li.find('div', class_='title').get_text().strip(), end=" ")
+                # print(li.find('div', class_='title').get_text().strip(), end=" ")
+                # print(li.find('span', class_='sale-price-low').get_text().strip())
+                _brand = li.find('div', class_='brand').get_text().strip()
+                _name = li.find('div', class_='title').get_text().strip().replace('/', '')
+                _price = li.find('span', class_='retail-price').get_text().strip().split("$")[1].replace(',','')
+                _sale_price = li.find('span', class_='sale-price-low').get_text().strip().split("$")[1].replace(',','')
+                _thumnail = li.find('span', class_='productBrowseMainImage').find('img')['src'].strip()
+                _url = f"https://www.shopbop.com/{li.find('a')['href']}"
+                _feature = kategorie_data[kategorie][1]
+                _color = random.choice(color_data)
+                print(_price,_sale_price)
+                _discount_rate = 100 - (int((float(_sale_price) / float(_price)) * 100))
+                # 이미지 다운로드
+                # urllib.request.urlretrieve(img, f"static/{kategorie}/{title}.jpg") # 이미지 다운로드
+                # print(f"{n}/{number} 다운로드 중")
+                n += 1
+                Product(name = _name, brand = _brand, price = float(_price), sale_price = float(_sale_price), discount_rate= _discount_rate, url= _url, thumnail = _thumnail, category = _feature, color = _color).save()
+
+def get_cloth_count(kategorie):
+    result = requests.get(f"https://www.shopbop.com/sale-clothing-{kategorie}/br/v=1/{kategorie_data[kategorie][0]}.htm")
+    src = result.content
+    soup = BeautifulSoup(src, 'lxml')
+    number = soup.find("div", class_="results").get_text()
+    pagination = int(number.split()[0]) // 100 + 1
+    return pagination
 
 def get6pmCrawler():
     categorys= {
@@ -67,97 +131,142 @@ def get6pmCrawler():
                 _discount_rate = "0"
   
             Product(name = _name, brand = _brand, price = float(_price), sale_price = float(_sale_price_text), discount_rate= _discount_rate, url= _url, thumnail = _thumnail, category = _feature, color = _color).save()
-            
-            
+                     
 def getFarfetchCrawler():
-    category_to_code = {
-    "Denim": "136043",
-    "Dresses": "135979",
-    "Shorts": "136045",
-    "Skirts": "135985",
-    "Jumpsuits": "136253",
-    "Jackets": "136226",
-    "Coats": "136227",
-    "Tops": "135983",
-    "Trousers": "135981",
+    code_code_to_category = {
+        # "니트웨어": "136245",
+        "136043" : "Denim",
+        "135979" : "Dresses",
+        "136045" : "Shorts",
+        "135985" : "Skirts",
+        "136253" : "Jumpsuits",
+        "136226" : "Jackets",
+        "136227" : "Coats",
+        "135983" : "Tops",
+        "135981" : "Trousers",
     }
 
-    custom_header = {
-        "referer": "https://www.farfetch.com/uk",
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-    }
+    #웹사이트 의류 크롤링 
+    def crawl_website():
+        for code in code_code_to_category.keys():
+            get_clothes_data(code)
+
+        return "finish"
 
 
-    def check_last_page(amount_item: int):
-        if amount_item != 90:
-            return True
-        else:
-            return False
+    # 카테고리별 크롤링
+    def get_clothes_data(code):
+        # 페이지 수 아직 오류가 있어 / MVP 이후 새로 구현필요
+        last_page_number = count_page(code)
+        data = []
+        category = code_code_to_category[code]
+        print(category)
+
+        # 페이지별 크롤링
+        for i in range(last_page_number):
+            print(f"page - start: {i} / {last_page_number}")
+            url = f"https://www.farfetch.com/uk/shopping/women/sale/all/items.aspx?page={i}&view=90&code={code}"
+            soup = crawl_page(url)
+
+            product_grid = soup.find("ul", {"data-testid": "product-card-list"})
+            products = product_grid.find_all("li", {"data-testid": "productCard"})
+            cnt = 0
+
+            # 상품별 크롤링
+            for product in products:
+                cnt += 1
+                len_product = len(products)
+                #페이지당 상품개수 및 크롤링중인 아이템 확인
+                print(f"item start: {cnt} / {len_product}")
+                if cnt % 39 == 0:
+                    time.sleep(0.002)
+                else:
+                    time.sleep(random.random() / 1000)
+
+                brand = product.find(
+                    "h3", {"data-testid": "productDesignerName"}
+                ).get_text()
+                item_detail = product.find(
+                    "p", {"data-testid": "productDescription"}
+                ).get_text()
 
 
-    def download_product_image(image_url: str, item_detail: str):
-        static_url = "/" + item_detail + ".jpg"
-        urllib.request.urlretrieve(image_url, static_url)
+                #영국 파운드화 기준으로 크롤링됨, 한국 환율 기준으로 변동 필요
+                price = product.find("span", {"data-testid": "price"}).get_text()
+                price = int(re.findall("\d+", price.replace(",", ""))[0]) * 1600
+                original_price = product.find(
+                    "span", {"data-testid": "initialPrice"}
+                ).get_text()
+                original_price = int(re.findall("\d+", original_price.replace(",", ""))[0]) * 1600
+                sale = math.floor((original_price - price) / original_price * 100)
+
+                product_url = (
+                    "https://www.farfetch.com" + product.find("a")["href"].lstrip()
+                )
+
+                image_url = product.find("meta")["content"]
+                #이미지 크롤링 실패서, continue
+                try:
+                    image_url = product.find("meta")["content"]
+                    image_static_url = download_image(image_url, brand, item_detail)
+                except:
+                    continue
+                data.append(
+                    {
+                        "category": category,
+                        "brand": brand,
+                        "item_detail": item_detail,
+                        "price(KWR)": price,
+                        "original_price(KWR)": original_price,
+                        "sale": sale,
+                        "product_url": product_url,
+                        "image_url": image_url,
+                    }
+                )
+                print(data)
+            return data
+
+
+    def crawl_page(url):
+        custom_header = {
+            "referer": url,
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+        }
+
+        req = requests.get(url, headers=custom_header)
+        req.raise_for_status()
+        soup = BeautifulSoup(req.content, "lxml")
+
+        return soup
+
+
+    def count_page(code):
+        url = f"https://www.farfetch.com/uk/shopping/women/sale/all/items.aspx?page=1&view=90&code={code}"
+        soup = crawl_page(url)
+        try:
+            max_page = max(
+                re.find(
+                    "\d+", (soup.find("div", {"data-testid": "page-number"}).get_text())
+                )
+            )
+            print(max_page)
+            return int(max_page)
+        except:
+            return 1
+
+
+    def download_image(img_url, str_brand, str_item_detail):
+        dir = "/Users/sangjoon/Downloads/crawling/"
+        static_url = dir + str_brand.replace(" ", "") + str_item_detail.replace(" ", "") + ".jpg"
+        urllib.request.urlretrieve(img_url, static_url)
+
         return static_url
 
 
-    def get_category_data(category_name: str, category_code: int):
-        pagination = 1
-        last_page = False
-        category_data = []
-
-        while not last_page:
-            url = f"https://www.farfetch.com/uk/shopping/women/sale/all/items.aspx?page={pagination}&view=90&code={category_code}"
-            req = requests.get(url, headers=custom_header)
-            soup = BeautifulSoup(req.content, "lxml")
-            products = soup.find_all("li", {"data-testid": "productCard"})
-            amount_item = len(products)
-
-            if check_last_page(amount_item):
-                last_page = True
-
-            for product in products:
-                product_brand = product.find("h3", {"data-testid": "productDesignerName"}).get_text()
-                product_detail = product.find("p", {"data-testid": "productDescription"}).get_text()
-                product_sale_price = product.find("span", {"data-testid": "price"}).get_text()
-                product_sale_price = int(product_sale_price[1:].replace(",", ""))
-                product_original_price = product.find(
-                    "span", {"data-testid": "initialPrice"}
-                ).get_text()
-                product_original_price = int(product_original_price[1:].replace(",", ""))
-                product_sale_rate = (
-                    (product_original_price - product_sale_price) / product_sale_price * 100
-                )
-                adjusted_sale_rate = math.floor(product_sale_rate)
-                product_url = "https://www.farfetch.com" + product.find("a")["href"].lstrip()
-                product_image_url = product.find("meta")["content"]
-
-                # download_product_image(product_image_url, product_detail)
-                category_data.append(
-                    [
-                        category_name,
-                        product_brand,
-                        product_detail,
-                        product_sale_price,
-                        product_original_price,
-                        adjusted_sale_rate,
-                        product_url,
-                        product_image_url,
-                    ]
-                )
-
-            pagination += 1
-
-        return category_data
-
-    def main():
-        for category, code in category_to_code.items():
-            category_data = get_category_data(category, code)
-            print(category, category_data)
-
+    crawl_website()
 
 def getJcrewCrawler():
     start = time.time()
@@ -240,5 +349,5 @@ def getJcrewCrawler():
 
 
 if __name__=='__main__':
-    get6pmCrawler()
+    get_cloth_data()
     
