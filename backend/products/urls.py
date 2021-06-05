02324b5e478dc0348739ac42dc587753c241ebbf
HyunGu Jeong <jhg3522@kookmin.ac.kr>
2021-06-05 10:58:24 +0000

    Feature/back recommend

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductView, LikeProductView
+from .views import ProductView, LikeProductView, RecommendView
 
 app_name = 'product'
 
 urlpatterns = [
     path('fashion/', ProductView.as_view(), name='fashion'),
     path('likeproduct/', LikeProductView.as_view(), name='likeproduct'),
+    path('recommend/', RecommendView.as_view(), name='likeproduct'),
 ]
