f4ccc84266c953a1d57c6a997240c283c418498a
HyunGu Jeong <jhg3522@kookmin.ac.kr>
2021-06-09 07:41:33 +0000

    Feature/back codereview

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,20 +1,20 @@
-import random
 import json
 from .models import Product, LikeProduct
-from style.models import Style
 from .serializers import ProductSerializer, LikeSerializer
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import serializers, status, generics
+from rest_framework import  generics
 from django_filters import rest_framework as filters
 from django.http import HttpResponse, JsonResponse
 from .pagination import CustomResultsSetPagination
 
 class ProductView(generics.ListAPIView):
+    """
+    url주소에서 category와 page_size를 받아 상품을 출력해주는 api
+    :param url: 불러올 url
+    :return:
+    """
     model = Product
     queryset = Product.objects.get_queryset().order_by('?')
     serializer_class = ProductSerializer
@@ -27,63 +27,76 @@ class ProductView(generics.ListAPIView):
 
 
 class RecommendView(generics.ListAPIView):
+    '''
+    상품의 feature와 color을 받아 그와 맞는 상품을 출력해주는 api
+    body : list안에 상품마다 feature,color를 딕셔너리 형태로 보내줌
+    ex)
+        [
+            {
+                "shorts" : "pink",
+                "long sleeve outwear" : "yellow"
+            },
+            {
+                "long sleeve dress" : "yellow"
+            }
+        ]
+    return : feature,color값에 대한 Product 정보
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [
         AllowAny
     ]
     pagination_class = CustomResultsSetPagination
-    filter_backends = (filters.DjangoFilterBackend,) 
-    filter_fields = ('category',)
 
-    def post(self, request):
+    def get(self, request):
         recommend_list = []
         request = (json.loads(request.body))
-        style_list = [i for i in request['styles']]
-        for i in style_list:
-            style = list(Style.objects.filter(id=i).values_list())[0][2]
-            for j in style:
-                _category = j
-                _color = style[_category]
-                recommend_list.append(list(Product.objects.filter(category=_category,color=_color).values()))
+        print(request)
+        for style in request:
+            for category, color in style.items():
+                print(category,color)
+                recommend_product = Product.objects.filter(category = category, color = color)
+                print(list(recommend_product.values()))
+                recommend_list.append(list(recommend_product.values()))
+            
+        
         return JsonResponse({'recommend_list': recommend_list})
 
-
-
-
-class LikeProductView(APIView):
+class LikeProductView(generics.ListAPIView):
+    '''
+    - post 요청시
+    상품의 좋아요 상태를 추가,삭제하는 api
+    body : 좋아요 할 post_id ()
+    ex)
+        "post_id" : 5352
+    return : message
+    
+    -get 요청시
+    jwt 토큰에 있는 유저정보 값으로 likeproduct 출력 api
+    return : likeproduct list 출력
+    '''
     model = LikeProduct
     serializer_class = LikeSerializer
-    authentication_classes = (JSONWebTokenAuthentication,)
     permission_classes = [
         IsAuthenticated
     ]
 
-    def post(self, request, *args, **kwargs):
-        user_data = json.loads(request.body)
-        print(user_data, request.user.pk)
-        if not LikeProduct.objects.filter(product_id=user_data['id'], user_id=request.user.pk).exists():
+    def post(self, request, format=None):
+        if not LikeProduct.objects.filter(product_id=request.data['product_id'], user_id=request.user.pk).exists():
             LikeProduct.objects.create(
                 user_id=request.user,
-                product_id=Product.objects.get(id=user_data['id']),
+                product_id=Product.objects.get(id=request.data['product_id']),
                 is_like=True
             ).save
-            return HttpResponse('good', status=200)
+            return HttpResponse({'message': 'CREATE_LIKE_PRODUCT'}, status=200)
 
-        delete_product = LikeProduct.objects.get(product_id=user_data['id'], user_id=request.user.pk)
+        delete_product = LikeProduct.objects.get(product_id=request.data['product_id'], user_id=request.user.pk)
         delete_product.delete()
-
         return HttpResponse({'message': 'DELETE_LIKE_PRODUCT'}, status=200)
 
-    def get(self, request, *args, **kwargs):
-        like_list = []
-        like_products = LikeProduct.objects.filter(user_id=request.user.pk).values_list()
-        for like_product in like_products:
-            products = Product.objects.filter(id=str(like_product[2]))
-            is_like_list = LikeProduct.objects.filter(product_id=str(like_product[2])).values_list()
-            is_like = (list(is_like_list)[0][3])
-            product_list = list(products.values())
-            product_list[0]['likeproduct'] = is_like
-            like_list.append(product_list[0])
 
-        return JsonResponse({'like_list': like_list})
+    def get(self, request, format=None):
+        queryset =LikeProduct.objects.filter(user_id=request.user.pk)
+        serializer = LikeSerializer(queryset, many=True)
+        return Response(serializer.data)
